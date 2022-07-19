a417370aff4005c74f6d799626c48d52f2e0638b
Bouhm <bbouhm@gmail.com>
2022-07-18 21:16:30 -0700

    update build

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,8 @@ const config = {
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
+			pages: "docs",
+			assets: "docs",
 			fallback: null,
 		}),
 		paths: {
